f2328297b173d3a18583636724d1f1866c9f91f7
Pedro Vieira <pedro.silva@pagtel.com.br>
2024-05-07 14:02:08 -0300

    refactor: plus minus optimization

--- plus_minus.py
+++ plus_minus.py
@@ -1,17 +1,22 @@
+def getRatio(num, arr_len):
+  return format(num / arr_len, ".6f")
+
 def plusMinus(arr):
   arr_lenght = len(arr)
-  positives = []
-  negatives = []
-  zeros = 0
+  positives = negatives = zeros = 0
 
   for number in arr:
     if number < 0:
-      negatives.append(number)
+      negatives += 1
     elif number > 0 :
-      positives.append(number)
+      positives += 1
     else:
       zeros += 1
 
-  return print(f'{format(len(positives) / arr_lenght, ".6f")}\n{format(len(negatives) / arr_lenght, ".6f")}\n{format(zeros / arr_lenght, ".6f")}') 
+  positive_ratio = getRatio(positives, arr_lenght)
+  negative_ratio = getRatio(negatives, arr_lenght)
+  zeros_ratio = getRatio(zeros, arr_lenght)
 
-plusMinus([-4, 3, -9, 0, 4, 1])
+  return f"{positive_ratio}\n{negative_ratio}\n{zeros_ratio}"
+
+print(plusMinus([-4, 3, -9, 0, 4, 1]))
